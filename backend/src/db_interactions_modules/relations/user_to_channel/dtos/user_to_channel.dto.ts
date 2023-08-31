fdbcbc74f63aeb351de26a85f7fcc4fe4d4b42d6
Dmarceli <daniel.sequeira@skeyra.pt>
2023-08-31 15:35:09 +0100

    Remove anys on user to channel

--- backend/src/db_interactions_modules/relations/user_to_channel/dtos/user_to_channel.dto.ts
+++ backend/src/db_interactions_modules/relations/user_to_channel/dtos/user_to_channel.dto.ts
@@ -1,24 +1,11 @@
 import { IsString, MaxLength,MinLength, ValidateIf, IsIn, IsNumber, isNumber, maxLength, IsDefined, minLength, IsBoolean } from 'class-validator';
 
   export class CreateUserToChannDto {
-
     @IsNumber()
-    user_id: number;
+    id: number;
 
-    @IsNumber()
-    channel_id: number;
-
-    @IsBoolean()
-    is_owner: boolean;
-
-    @IsBoolean()
-    is_admin: boolean;
-
-    @IsBoolean()
-    is_muted: boolean;
-
-    @IsBoolean()
-    is_banned: boolean;
+    @IsString()
+    pass: string;
 
   }
   
